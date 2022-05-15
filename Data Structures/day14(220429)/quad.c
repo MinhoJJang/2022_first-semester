110445fde33c2ce618ab3ae09a1a610f57818714
MinhoJJang <ahchjang@naver.com>
2022-05-15 16:52:50 +0900

    0515

--- Data Structures/day14(220429)/quad.c
+++ Data Structures/day14(220429)/quad.c
@@ -2,6 +2,7 @@
 #include <time.h>
 #include <malloc.h>
 #include <stdio.h>
+#include <stdlib.h>
 /*
     quad tree 만들기
 
@@ -13,6 +14,8 @@
 
 #define MAP_SIZE 21
 #define NODATA 0
+#define NO 0
+#define YES 1
 
 // 점을 저장하기 위한 구조체
 typedef struct _Point
@@ -71,8 +74,6 @@ void tree_init(Tree *tree)
     tree->top_left = NULL;
     tree->top_right = NULL;
 
-    printf("%d", tree->map->center->x);
-
     return;
 }
 
@@ -80,7 +81,9 @@ void tree_init(Tree *tree)
 #define TL 1
 #define BL 2
 #define BR 3
-#define MAX 100
+#define MAX 400
+
+int numberOfPrinted = 1;
 
 // 해당 MAP 안에 몇개의 점이 존재하는지 파악하는 함수. 점이 1개일 경우 분열하지 않고, 점이 2개 이상일 경우 분열한다. 만약 0개면 아무것도 하지 않으면 된다.
 void checkPoints(Tree *tree, Point data[])
@@ -91,6 +94,7 @@ void checkPoints(Tree *tree, Point data[])
     // map 내부에 점이 0개일 경우
     if (numberOfPoints == NODATA)
     {
+        tree = NULL;
         return;
     }
     else if (numberOfPoints == 1) // 점이 1개인 경우 더이상의 탐색은 종료하며 tree에 데이터를 넣는다.
@@ -100,7 +104,7 @@ void checkPoints(Tree *tree, Point data[])
         tree->map->point->x = data[0].x;
         tree->map->point->y = data[0].y;
 
-        printf("x: %d, y: %d\n", tree->map->point->x, tree->map->point->y);
+        printf("%d point -> x: %d, y: %d\n", numberOfPrinted++, tree->map->point->x, tree->map->point->y);
     }
     else if (numberOfPoints > 1)
     {
@@ -156,28 +160,132 @@ void checkPoints(Tree *tree, Point data[])
             }
         }
 
-        tree->top_right = newTree[TR];
-        tree->top_left = newTree[TL];
-        tree->bottom_left = newTree[BL];
-        tree->bottom_right = newTree[BR];
+        if (numberOf[TR] > 0)
+        {
+            tree->top_right = newTree[TR];
+            tree->top_right->map = init_map(tree->map->center->x, tree->map->center->y, tree->map->top_right->x, tree->map->top_right->y);
+            checkPoints(tree->top_right, map_data[TR]);
+        }
 
-        tree->top_right->map = init_map(tree->map->center->x, tree->map->center->y, tree->map->top_right->x, tree->map->top_right->y);
-        tree->top_left->map = init_map(tree->map->bottom_left->x, tree->map->center->y, tree->map->center->x, tree->map->top_right->y);
-        tree->bottom_left->map = init_map(tree->map->bottom_left->x, tree->map->bottom_left->y, tree->map->center->x, tree->map->center->y);
-        tree->bottom_right->map = init_map(tree->map->center->x, tree->map->bottom_left->y, tree->map->top_right->x, tree->map->center->y);
+        if (numberOf[TL] > 0)
+        {
+            tree->top_left = newTree[TL];
+            tree->top_left->map = init_map(tree->map->bottom_left->x, tree->map->center->y, tree->map->center->x, tree->map->top_right->y);
+            checkPoints(tree->top_left, map_data[TL]);
+        }
 
-        checkPoints(tree->top_right, map_data[TR]);
-        checkPoints(tree->top_left, map_data[TL]);
-        checkPoints(tree->bottom_left, map_data[BL]);
-        checkPoints(tree->bottom_right, map_data[BR]);
+        if (numberOf[BL] > 0)
+        {
+            tree->bottom_left = newTree[BL];
+            tree->bottom_left->map = init_map(tree->map->bottom_left->x, tree->map->bottom_left->y, tree->map->center->x, tree->map->center->y);
+            checkPoints(tree->bottom_left, map_data[BL]);
+        }
+
+        if (numberOf[BR] > 0)
+        {
+            tree->bottom_right = newTree[BR];
+            tree->bottom_right->map = init_map(tree->map->center->x, tree->map->bottom_left->y, tree->map->top_right->x, tree->map->center->y);
+
+            checkPoints(tree->bottom_right, map_data[BR]);
+        }
     }
-
     return;
 }
 
-void searchPoint(int x, int y)
+void searchPoint(Tree *tree, Point *point)
 {
-    // TODO
+    // root node 부터, node의 point 값이 존재할 경우 비교한다. 값이 없을 경우, map의 center 포인트를 기준으로 포인트의 위치를 1사분면부터 4사분면까지 잡는다. 그리고 그 사분면으로 이동하여 같은 로직을 수행한다. 만약 이동하는 사분면에 아무것도 존재하지 않는 NULL 상태인 경우 search fail 이다.
+
+    // 해당 tree의 map에 point값이 존재하는가? 만약 값이 존재한다는 것은 그곳이 leaf 노드라는 뜻이고 여기서 못찾으면 그냥 search fail이다.
+    if (tree->map->point != NULL)
+    {
+        // 이곳은 leaf node이며 quad tree 정의에 따라 이곳이 탐색의 마지막이다.
+        if (tree->map->point->x == point->x && tree->map->point->y == point->y)
+        {
+            // search success
+            printf("search success! point(%d, %d)\n", point->x, point->y);
+        }
+        else
+        {
+            // search fail
+            printf("search fail\n");
+        }
+        return;
+    }
+    else
+    {
+        int center_x = tree->map->center->x;
+        int center_y = tree->map->center->y;
+        int x = point->x;
+        int y = point->y;
+        int isPointerNull = NO;
+
+        if (center_x <= x && center_y <= y)
+        {
+            if (tree->top_right != NULL)
+            {
+                searchPoint(tree->top_right, point);
+            }
+            else
+            {
+                isPointerNull = YES;
+            }
+        }
+        else if (x < center_x && center_y <= y)
+        {
+            if (tree->top_left != NULL)
+            {
+                searchPoint(tree->top_left, point);
+            }
+            else
+            {
+                isPointerNull = YES;
+            }
+        }
+        else if (x < center_x && y < center_y)
+        {
+            if (tree->bottom_left != NULL)
+            {
+                searchPoint(tree->bottom_left, point);
+            }
+            else
+            {
+                isPointerNull = YES;
+            }
+        }
+        else
+        {
+            if (tree->bottom_right != NULL)
+            {
+                searchPoint(tree->bottom_right, point);
+            }
+            else
+            {
+                isPointerNull = YES;
+            }
+        }
+
+        if (isPointerNull == YES)
+        {
+            printf("search fail\n");
+            return;
+        }
+    }
+}
+
+void CheckSameData(Point *point, int *idx, int *reset)
+{
+    int num = *idx;
+    int x = point[num].x;
+    int y = point[num].y;
+    for (int i = 0; i < num; i++)
+    {
+        if (point[i].x == x && point[i].y == y)
+        {
+            *reset = YES;
+            return;
+        }
+    }
 }
 
 int main()
@@ -190,12 +298,26 @@ int main()
     scanf_s("%d", &NumberOfPoint);
     Point *input_data = (Point *)malloc(sizeof(Point) * NumberOfPoint); //동적할당
 
-    // 값 임의지정
+    srand(time(NULL));
+    //값 임의지정
     for (int i = 0; i < NumberOfPoint; i++)
     {
-        input_data[i].x = i + 3;
-        input_data[i].y = i + 1;
+        int reset = NO;
+        input_data[i].x = rand() % 20 + 1;
+        input_data[i].y = rand() % 20 + 1;
+
+        CheckSameData(input_data, &i, &reset);
+        if (reset == YES)
+        {
+            i--;
+        }
     }
+
+    // input_data[0].x = 3;
+    // input_data[0].y = 3;
+
+    // input_data[1].x = 15;
+    // input_data[1].y = 3;
 
     for (int i = 0; i < NumberOfPoint; i++)
     {
@@ -203,6 +325,16 @@ int main()
     }
 
     checkPoints(&tree, input_data);
+
+    printf("===========Search Point Test============\n");
+    int input_x;
+    int input_y;
+    printf("Which point do you want to search? : ");
+    scanf_s("%d%d", &input_x, &input_y);
+    printf("Search Point : (%d, %d)\n", input_x, input_y);
+    Point *newPoint = (Point *)malloc(sizeof(Point));
+    newPoint = init_point(input_x, input_y);
+    searchPoint(&tree, newPoint);
 
     return 0;
 }
